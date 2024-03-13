016a367b151f9511032fb29b64cb7c70bb06f708
EMCodeStudio <estivenmc53@gmail.com>
2024-03-13 16:41:08 -0500

    change params names

--- Ecuaciones_AN.py
+++ Ecuaciones_AN.py
@@ -46,7 +46,7 @@ def formato_metodo_grafico():
     plt.plot(x, [calcular_f(a, b, c, i) for i in x])
 
     plt.axhline(0, color="red")
-    plt.axvline(0, color="black")
+    plt.axvline(0, color="red")
 
     plt.xlim(-10, 10)
     plt.ylim(-10, 10)
@@ -97,7 +97,7 @@ def formato_tabla_biseccion(limite_a, limite_b, results):
         f_a = calcular_f(a, b, c, limite_a)
         f_b = calcular_f(a, b, c, limite_b)
 
-        # Calcular el porcentaje de error usando el punto medio actual y el anterior
+        # Calcular el porcentaje de error usando el xr actual y el anterior
         if i > 0:
             porcentaje_error = abs((pm - results[i - 1][5]) / pm) * 100
         else:
@@ -105,7 +105,16 @@ def formato_tabla_biseccion(limite_a, limite_b, results):
         tree.insert(
             "",
             "end",
-            values=(i + 1, limite_a, f_a, pm, f_pm, limite_b, f_b,f"{porcentaje_error:.2f}"),
+            values=(
+                i + 1,
+                limite_a,
+                f_a,
+                pm,
+                f_pm,
+                limite_b,
+                f_b,
+                f"{porcentaje_error:.2f}",
+            ),
         )
     tree.pack(fill="both", expand=True)
 
@@ -122,43 +131,55 @@ def metodo_biseccion():
 
         maxima_iteracion = int(max_iteracion.get())
 
-        results = valores_metodo_biseccion(a, b, c, limite_a, limite_b, maxima_iteracion)
+        results = valores_metodo_biseccion(
+            a, b, c, limite_a, limite_b, maxima_iteracion
+        )
         if results:
             formato_tabla_biseccion(limite_a, limite_b, results)
     except ValueError:
         messagebox.showerror(
             "Error", "Los coeficientes ingresados no son números validos!"
         )
-        
-        
-        # Funcion para calcular los resultados con el Metodo de Falsa Posicion
-def valores_metodo_falsa_posicion(a, b, c, limite_a, limite_b, maxima_iteracion):
+
+
+# Funcion para calcular f con la formula de la Ecuacion
+def calcular_f_falsa_posicion(x):
+    return -25 + 82 * x - 90 * x**2 + 44 * x**3 - 8 * x**4 + 0.7 * x**5
+    print("Valores de x: " + x)
+
+
+# Funcion para calcular los resultados con el Metodo de Falsa Posicion
+def valores_metodo_falsa_posicion(limite_xl, limite_xu, maxima_iteracion):
     contador_iteracion = 0
     results = []
-    if calcular_f(a, b, c, limite_a) * calcular_f(a, b, c, limite_b) > 0:
-        messagebox.showerror("Error", "No hay cambio de signo en el intervalo dado.")
-        return []
-
+    ##if calcular_f(a, b, c, limite_a) * calcular_f(a, b, c, limite_b) > 0:
+    ##messagebox.showerror("Error", "No hay cambio de signo en el intervalo dado.")
+    ##return []
     while contador_iteracion < maxima_iteracion:
-        results.append((a, b, c, limite_a, limite_b))
-        
-        f_xl = calcular_f(a, b, c, limite_a)
-        f_xu = calcular_f(a, b, c, limite_b)
-        xr = limite_b - (f_xu * (limite_a - limite_b) / (f_xl - f_xu))
-        f_xr = calcular_f(a, b, c, 2)
-        
-        if calcular_f(a, b, c, limite_a) * f_xr > 0:
-            limite_a = xr
+
+        f_xl = calcular_f_falsa_posicion(limite_xl)
+        f_xu = calcular_f_falsa_posicion(limite_xu)
+
+        xr = limite_xu - (f_xu * (limite_xl - limite_xu)) / (f_xl - f_xu)
+
+        f_xr = calcular_f_falsa_posicion(xr)
+
+        results.append((limite_xl, limite_xu, f_xl, f_xu, xr, f_xr))
+
+        if calcular_f_falsa_posicion(limite_xl) * f_xr > 0:
+            limite_xl = xr
         else:
-            limite_a = limite_a
-        if calcular_f(a, b, c, xr) * f_xu < 0:
-            limite_b = limite_b
+            limite_xl = limite_xl
+
+        if calcular_f_falsa_posicion(xr) * f_xu < 0:
+            limite_xu = limite_xu
         else:
-            limite_b = xr 
+            limite_xu = xr
+
         contador_iteracion += 1
     return results
 
-        
+
 # Funcion para dar formato a los valores del metodo de Falsa Posicion en una tabla
 def formato_tabla_falsa_posicion(limite_a, limite_b, results):
     results_window = tk.Toplevel(root)
@@ -172,52 +193,55 @@ def formato_tabla_falsa_posicion(limite_a, limite_b, results):
         "f(xu)",
         "xr",
         "f(xr)",
-        "Error %",
+        "Error a%",
     )
     for column in tree["columns"]:
         tree.heading(column, text=column)
-    for i, (a, b, c, limite_a, f_xl, limite_b , f_xu , xr , f_xr ) in enumerate(results):
-        
-        porcentaje_error_aproximado = 0 
-    
+    for i, (limite_xl, limite_xu, f_xl, f_xu, xr, f_xr) in enumerate(results):
+        print("ORIGINAL XU", limite_xu)
         # Calcular el porcentaje de error usando el punto medio actual y el anterior
-        ## if i > 0:
-        ##    porcentaje_error_aproximado = abs((pm - results[i - 1][5]) / pm) * 100
-        ## else:
-           ##porcentaje_error_aproximado = 0  # No hay error en la primera iteración
-           
+        if i > 0:
+            porcentaje_error_aproximado = (
+                abs((limite_xu - results[i - 1][4]) / limite_xu) * 100
+            )
+            print("XU", limite_xu)
+            print("POSICIONES", results[i - 1][4])
+        else:
+            porcentaje_error_aproximado = 0  # No hay error en la primera iteración
         tree.insert(
             "",
             "end",
-            values=(i + 1, limite_a, f_xl, limite_b, f_xu, xr, f_xr ,f"{porcentaje_error_aproximado:.2f}"),
+            values=(
+                i + 0,
+                limite_xl,  # XL
+                f_xl,
+                limite_xu,  # XU
+                f_xu,
+                xr,
+                f_xr,
+                f"{porcentaje_error_aproximado:.2f}",
+            ),
         )
     tree.pack(fill="both", expand=True)
 
-        
-def metodo_falsa_posicion ():
+
+def metodo_falsa_posicion():
     try:
-        a = float(valor_a.get())
-        b = float(valor_b.get())
-        c = float(valor_c.get())
-        
+
         limite_a = float(a_limite.get())
         limite_b = float(b_limite.get())
         maxima_iteracion = int(max_iteracion.get())
-        
-        results = valores_metodo_falsa_posicion(a, b, c, limite_a, limite_b, maxima_iteracion)
+
+        results = valores_metodo_falsa_posicion(limite_a, limite_b, maxima_iteracion)
         if results:
             formato_tabla_falsa_posicion(limite_a, limite_b, results)
     except ValueError:
-        messagebox.showerror(
-        "Error", "Los coeficientes ingresados no son números validos!"
-     )            
-                
-                
-                
-            
+        messagebox.showerror("Error", "Los datos ingresados no son números validos!")
+
+
 # Crear la Interfaz
 root = tk.Tk()
-root.title("Ecuaciones Analisis Cuadratico")
+root.title("Ecuaciones Analisis Numerico")
 
 # Campos de Entrada de Valores de los Coeficientes
 valor_a = ttk.Entry(root)
@@ -228,8 +252,8 @@ valor_c = ttk.Entry(root)
 valor_c.grid(row=0, column=5, padx=5, pady=5)
 
 # Etiquetas de los Campos de Entrada de Valores
-ttk.Label(root, text="x^2 +").grid(row=0, column=2, padx=5, pady=5)
-ttk.Label(root, text="x +").grid(row=0, column=4, padx=5, pady=5)
+ttk.Label(root, text="X^2 +").grid(row=0, column=2, padx=5, pady=5)
+ttk.Label(root, text="X +").grid(row=0, column=4, padx=5, pady=5)
 ttk.Label(root, text="= 0").grid(row=0, column=6, padx=5, pady=5)
 
 # Campos de Entrada de Valores de los Limites de la Iteracion
@@ -244,8 +268,8 @@ max_iteracion.grid(row=3, column=5, padx=5, pady=5)
 
 
 # Etiquetas de los Campos de Entrada de Limites de Iteracion
-ttk.Label(root, text="xi").grid(row=3, column=2, padx=5, pady=5)
-ttk.Label(root, text="xu").grid(row=3, column=4, padx=5, pady=5)
+ttk.Label(root, text="Xi").grid(row=3, column=2, padx=5, pady=5)
+ttk.Label(root, text="Xu").grid(row=3, column=4, padx=5, pady=5)
 ttk.Label(root, text="Max Iteraciones").grid(row=3, column=6, padx=5, pady=5)
 
 
@@ -256,15 +280,13 @@ btnMetodoCuadratico = ttk.Button(
 btnMetodoCuadratico.grid(row=1, column=0, columnspan=2, pady=10)
 
 # Ejecutar Metodo Grafico
-btnMetodoCuadratico = ttk.Button(
+btnMetodoGrafico = ttk.Button(
     root, text="Metodo Grafico", command=formato_metodo_grafico
 )
-btnMetodoCuadratico.grid(row=1, column=2, columnspan=2, pady=10)
+btnMetodoGrafico.grid(row=1, column=2, columnspan=2, pady=10)
 
 # Ejecutar Metodo Biseccion
-btnMetodoBiseccion = ttk.Button(
-    root, text="Metodo Biseccion", command=metodo_biseccion
-)
+btnMetodoBiseccion = ttk.Button(root, text="Metodo Biseccion", command=metodo_biseccion)
 btnMetodoBiseccion.grid(row=5, column=2, columnspan=2, pady=10)
 
 # Ejecutar Metodo Falsa Posicion
